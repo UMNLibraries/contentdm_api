c87518dc9f50e4cf769b5738dc43cbff5e87c9b2
Chad Fennell <fenne035@umn.edu>
2016-07-12 13:06:59 -0500

    expose the full api

--- lib/contentdm.rb
+++ lib/contentdm.rb
@@ -2,4 +2,6 @@ require "contentdm/version"
 require 'net/http'
 require 'json'
 
+require_relative './contentdm/request'
 require_relative './contentdm/request_batch'
+require_relative './contentdm/service'
