c0e3d11dd11016f1ed4f173478ef7f41c2bf963c
chadfennell <libsys@gmail.com>
2016-12-01 20:59:32 -0700

    regression test for items with bad compound data

--- test/item_test.rb
+++ test/item_test.rb
@@ -29,6 +29,15 @@ module CONTENTdmAPI
           end
         end
       end
+
+      describe 'when an item presents an array of arrays for compounds instead of an array of hashes' do
+        it 'returns an empty compound' do
+          VCR.use_cassette('valid_item_request_with_invalid_compound') do
+            compound = Item.new(with_compound: false, base_url: 'https://server16022.contentdm.oclc.org/dmwebservices/index.php', collection: 'gust', id: 373)
+            compound.metadata.must_equal({"title"=>"Message of thanks from Saratov University to students of Gustavus Adolphus, St. Peter, Minnesota", "photog"=>"Students of Saratov University", "contri"=>"Wold & Baklund Portrait Gallery (Willmar, Minnesota)", "descri"=>"A drought, coupled with massive economic and social restructuring during the Russian Revolution, resulted in a devastating famine in 1921-1922 in the Volga region, which includes the City of Saratov.  Gustavus students and faculty responded to an appeal from the National Lutheran Council in late 1921 with a contribution of $395.25 toward Russian relief.  This letter of thanks, with parallel text in Russian and English, was written and illustrated by students of German ancestry enrolled at Saratov University.", "dat"=>"1922-05-07", "publia"=>{}, "dimens"=>"38.5 x 24", "genera"=>"Education", "type"=>"Text", "physic"=>"Letters (correspondence)", "specif"=>"College Students; Soviet Union History Revolution, 1917-1921;", "subjec"=>"Saratov State University; Relief; Outreach", "city"=>"St. Peter", "distri"=>{}, "county"=>"Nicollet", "state"=>"Minnesota", "countr"=>"United States", "geogra"=>{}, "geonam"=>"http://sws.geonames.org/5029064/", "latitu"=>{}, "longit"=>{}, "geogrb"=>{}, "langua"=>"English", "par"=>{}, "contra"=>"Gustavus Adolphus College", "contac"=>"Gustavus Adolphus College, 800 West College Avenue, St. Peter, MN 56082 http://gustavus.edu", "righta"=>"May not be published or extensively quoted without the permission of Gustavus Adolphus College.", "identi"=>"0000-0017-1", "resour"=>{}, "projec"=>"Minnesota Reflections 2005-06", "fiscal"=>"Grant provided to the Minnesota Digital Library Coalition through the Library Services and Technology Act (LSTA) and the State Library Services and School Technology unit of the Minnesota Department of Education", "publis"=>{}, "date"=>{}, "format"=>{}, "digspe"=>{}, "digspa"=>{}, "digspb"=>{}, "digspc"=>{}, "digspd"=>{}, "digspf"=>{}, "digspg"=>{}, "digsph"=>{}, "digspi"=>{}, "digspj"=>{}, "digspk"=>{}, "transc"=>{}, "transl"=>{}, "fullrs"=>{}, "find"=>"269.cpd", "dmaccess"=>{}, "dmimage"=>{}, "dmcreated"=>"2016-10-26", "dmmodified"=>"2016-10-26", "dmoclcno"=>{}, "dmrecord"=>"373", "restrictionCode"=>"1", "cdmfilesize"=>"213", "cdmfilesizeformatted"=>"0.00 MB", "cdmprintpdf"=>"0", "cdmhasocr"=>"0", "cdmisnewspaper"=>"0", "id"=>"gust/373", "page"=>[]})
+          end
+        end
+      end
     end
   end
 end
